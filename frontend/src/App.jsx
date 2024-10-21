587f23b11811abc57bc15bdf8d6f731c5343037b
Naved khan <nomanali50997@gmail.com>
2024-10-21 10:11:26 +0530

    desigining navbar and dropdown

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,24 @@ import Contact from "./pages/Contact"
 import MyProfile from "./pages/MyProfile"
 import MyApointments from "./pages/MyApointments"
 import Apointment from './pages/Apointment'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-
+      <Navbar />
       <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/doctors' element={<Doctors/>} />
-        <Route path='/doctors/:speciality' element={<Doctors/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-apointments' element={<MyApointments />} />
         <Route path='/apointment/:docId' element={<Apointment />} />
-        
+
       </Routes>
 
     </div>
