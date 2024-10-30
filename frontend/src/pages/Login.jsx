89ed202ea9500f86dd5817eb0d49d4900633d296
Naved khan <nomanali50997@gmail.com>
2024-10-30 08:26:36 +0530

    touch up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState('Sign Up')
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center ' >
+      <div>
+        <p>{state === 'Sign Up' ? "Create account" : "Login"} </p><br />
+        <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment </p>
+      </div>
+    </form>
   )
 }
 
