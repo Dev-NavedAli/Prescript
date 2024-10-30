3f01f83f603fca7e22cc8f638f9f70ee299e0c44
Naved khan <nomanali50997@gmail.com>
2024-10-30 08:42:42 +0530

    designed login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,9 +14,28 @@ const Login = () => {
 
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center ' >
-      <div>
-        <p>{state === 'Sign Up' ? "Create account" : "Login"} </p><br />
+      <div className='flex flex-col gap-3 m-auto p-8 min-w-[340px] sm:min-w-96 rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold '>{state === 'Sign Up' ? "Create account" : "Login"} </p>
         <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment </p>
+        <div className='w-full'>
+          <p>Full Name</p>
+          <input type="text" onChange={(e) => setName(e.target.value)} value={name} className='border border-zinc-300 rounded w-full p-2 mt-1' required />
+        </div>
+        <div className='w-full'>
+          <p>Email</p>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} className='border border-zinc-300 rounded w-full p-2 mt-1' required />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} className='border border-zinc-300 rounded w-full p-2 mt-1' required />
+        </div >
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? " Create Account " : "Log in"}</button>
+        <p>
+          {
+            state === 'Sign Up' ? <p>Already have an account ? log in here </p> 
+             : <p> Create a New account ? click here </p>
+          }
+        </p>
       </div>
     </form>
   )
