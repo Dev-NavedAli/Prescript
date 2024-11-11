4e0fd9d2c94c44a1025b4d56bde72c3ce38f03e3
Naved khan <nomanali50997@gmail.com>
2024-11-11 09:15:37 +0530

    configured middlewares

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+//app config
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("API working successfully");
