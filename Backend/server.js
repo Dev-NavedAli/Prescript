31963633163ce454c542418675df5d82f95c89ad
Naved khan <nomanali50997@gmail.com>
2024-11-12 13:08:27 +0530

    configuring controllers and routes

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors"
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 dotenv.config();
 
@@ -12,7 +13,14 @@ const PORT = process.env.PORT || 3000;
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+    origin: 'https://hoppscotch.io', // Allow requests from Hoppscotch
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true // If you need to allow cookies/auth headers
+  }));
 
 connectDB().then(()=>{
     console.log('connected to db successfully');
@@ -21,6 +29,8 @@ connectDB().then(()=>{
 })
 connectCloudinary()
 
+//api endpoints
+app.use('/api/admin',adminRouter)
 
 app.get("/", (req, res) => {
     res.send("API working Great");
