62aaffceca20adc7324575c56d0eb129aff77675
Naved khan <nomanali50997@gmail.com>
2024-11-21 10:50:53 +0530

    design all Navlink

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -18,17 +18,17 @@ const SideBar = () => {
             <p>Dashboard</p>
           </NavLink>
 
-          <NavLink to='/all-appointments'>
+          <NavLink to='/all-appointments' className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5F65FF]': ''}` }>
             <img src={assets.appointment_icon} alt="" />
             <p> Appoitments</p>
           </NavLink>
 
-          <NavLink to='/add-doctor'>
-            <img src={assets.add_icon} alt="" />
+          <NavLink to='/add-doctor' className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5F65FF]': ''}` }  >
+            <img src={assets.add_icon} alt=""  />
             <p>Add Doctor</p>
           </NavLink>
 
-          <NavLink to='/doctor-list'>
+          <NavLink to='/doctor-list' className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5F65FF]': ''}` }>
             <img src={assets.people_icon} alt="" />
             <p>Doctor List</p>
           </NavLink>
