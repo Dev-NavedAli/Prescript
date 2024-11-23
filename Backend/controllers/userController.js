b675cc1abbcf4e40f1402be91a3ea1c9e063d3b6
Naved khan <nomanali50997@gmail.com>
2024-11-23 16:03:55 +0530

    code the userController

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,56 @@
+//Api for register user
+import validator from "validator";
+import userModel from "../models/userModel.js";
+import bcrypt, { genSalt } from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !password || !email) {
+      res.json({ success: false, message: "missing details" });
+    }
+    
+    //validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "email is not valid" });
+    }
+
+    //validating the strength of password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password must be 8 character long",
+      });
+    }
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User Already exist" });
+    }
+
+    //hashing the password 
+
+    const salt  = await bcrypt.genSalt(10)
+    const hashed_password = await bcrypt.hash(password,salt);
+     
+    const userData =  {
+        name,
+        email,
+        password:hashed_password
+    }
+    
+    const newUser = new userModel(userData)
+    const  user = await newUser.save()
+    
+    const token =  jwt.sign({id:user._id},process.env.JWT_SECRET)
+    res.json({success:true , token})
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:error.message})
+    
+  }
+}
+
+export {registerUser}
