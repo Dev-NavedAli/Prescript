6ff3379b1a642145ede7409579d09e2d15bea4f3
Naved khan <nomanali50997@gmail.com>
2024-11-26 12:58:55 +0530

    make the login api

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,10 @@
 //Api for register user
 import validator from "validator";
 import userModel from "../models/userModel.js";
-import bcrypt, { genSalt } from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt, { genSalt } from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
 
 const registerUser = async (req, res) => {
   try {
@@ -10,7 +12,7 @@ const registerUser = async (req, res) => {
     if (!name || !password || !email) {
       res.json({ success: false, message: "missing details" });
     }
-    
+
     //validating email format
     if (!validator.isEmail(email)) {
       return res.json({ success: false, message: "email is not valid" });
@@ -28,29 +30,53 @@ const registerUser = async (req, res) => {
       return res.json({ success: false, message: "User Already exist" });
     }
 
-    //hashing the password 
+    //hashing the password
 
-    const salt  = await bcrypt.genSalt(10)
-    const hashed_password = await bcrypt.hash(password,salt);
-     
-    const userData =  {
-        name,
-        email,
-        password:hashed_password
-    }
-    
-    const newUser = new userModel(userData)
-    const  user = await newUser.save()
-    
-    const token =  jwt.sign({id:user._id},process.env.JWT_SECRET)
-    res.json({success:true , token})
+    const salt = await bcrypt.genSalt(10);
+    const hashed_password = await bcrypt.hash(password, salt);
 
+    const userData = {
+      name,
+      email,
+      password: hashed_password,
+    };
 
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: true, token });
   } catch (error) {
     console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Api for user Login
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return res.json({ success: false, message: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (isMatch) {
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+    res.json({ success: true, token });
+  } else {
+    res.json({ success: false, message: "Invalid Credentials" });
+  }
+  } catch (error) {
     res.json({success:false,message:error.message})
+    console.log(error);
     
   }
-}
+  
+};
 
-export {registerUser}
+export { registerUser, loginUser };
