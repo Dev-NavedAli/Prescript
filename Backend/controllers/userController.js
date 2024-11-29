ecb63ac2fb0fbafb778d8e4bba141af38f91f815
Naved khan <nomanali50997@gmail.com>
2024-11-29 12:09:01 +0530

    make a api that edit the user profile info in backend

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,8 +3,7 @@ import validator from "validator";
 import userModel from "../models/userModel.js";
 import bcrypt, { genSalt } from "bcrypt";
 import jwt from "jsonwebtoken";
-
-
+import {v2 as cloudinary} from 'cloudinary'
 
 const registerUser = async (req, res) => {
   try {
@@ -58,38 +57,65 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-  const user = await userModel.findOne({ email });
-  if (!user) {
-    return res.json({ success: false, message: "User not found" });
-  }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
 
-  const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-  if (isMatch) {
-    const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
-    res.json({ success: true, token });
-  } else {
-    res.json({ success: false, message: "Invalid Credentials" });
-  }
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
   } catch (error) {
-    res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message });
     console.log(error);
-    
   }
-  
 };
 
 //Api to get user Profile  data
 
-const getProfile = async(req,res)=>{
+const getProfile = async (req, res) => {
   try {
-    const {userId} = req.body
-    const userData = await userModel.findById(userId).select('-password')
-    res.json({success:true,userData})
+    const { userId } = req.body; //authUser middleware ki help se userId aari h
+    const userData = await userModel.findById(userId).select("-password");
+    res.json({ success: true, userData });
   } catch (error) {
-    res.json({success:false,message:error.message})
-    console.log(error)
+    res.json({ success: false, message: error.message });
+    console.log(error);
   }
-}
+};
 
-export { registerUser, loginUser ,getProfile };
+//Api to update user profile
+
+const updateProfile = async (req, res) => {
+  try {
+    const { userId, name, phone, address, dob, gender } = req.body;
+    const imageFile = req.file;
+
+    if (!name || !phone || !dob || !gender) {
+      return res.json({ success: false, message: "Data is missing " });
+    }
+    await userModel.findByIdAndUpdate(userId, {
+      name,
+      phone,
+      address: JSON.parse(address),
+      dob,
+      gender,
+    });
+    if(imageFile){
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+      const imageUrl  = imageUpload.secure_url
+      await userModel.findByIdAndUpdate(userId,{image:imageUrl})
+    }
+    res.json({success:true,message:'Profile Updated'})
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+    console.log(error);
+  }
+};
+
+export { registerUser, loginUser, getProfile , updateProfile};
