b838c813216d429686b87f81aba3eea8fee0cac0
Naved khan <nomanali50997@gmail.com>
2024-12-26 16:34:16 +0530

    making the apis for appointment Complete and cancel

--- Backend/routes/doctorRoute.js
+++ Backend/routes/doctorRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { doctorList ,loginDoctor , appointmentsDoctor } from "../controllers/doctorController.js"
+import { doctorList ,loginDoctor , appointmentsDoctor , appointmentCancel,
+appoinmentComplete, } from "../controllers/doctorController.js"
 import authDoctor from "../middlewares/authDoctor.js"
 
 const doctorRouter = express.Router()
@@ -8,5 +9,7 @@ const doctorRouter = express.Router()
 doctorRouter.get('/list',doctorList)
 doctorRouter.post('/login',loginDoctor)
 doctorRouter.get('/appointments',authDoctor,appointmentsDoctor)
+doctorRouter.post('/complete-appointments',authDoctor,appoinmentComplete)
+doctorRouter.post('/cancel-appointments',authDoctor,appointmentCancel)
 
 export default doctorRouter;
